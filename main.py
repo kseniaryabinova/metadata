5047bdab24bf82fb512624254258d0d877473b18
kseniaryabinova <ryabinova.ka@mail.ru>
2017-11-16 13:04:58 +0300

    парсинг xml->ram переделан с рекурсией

--- main.py
+++ main.py
@@ -1,145 +1,183 @@
 # дедлайн - первые 2 задания до 21 октября
 # 4 задача должна быть выложена до начала декабря
-from xml.dom.minidom import parse
-from metadata import *
-from minidom_fixed import *
+from xml.dom.minidom import parse, Element
+from minidom_fixed import Document
 from io import StringIO
+from metadata import *
 from custom_exception import *
 # TODO исправить import *
+# TODO переделать под рекурсию
+# for el1 in dom.childNodes:
+#     print('\t'+el1.tagName)
+#     for el2 in el1.childNodes:
+#         if type(el2) == Element:
+#             print('\t\t'+el2.tagName)
+#             for el3 in el2.childNodes:
+#                 if type(el3) == Element:
+#                     print('\t\t\t'+el3.tagName)
+#                     for el4 in el3.childNodes:
+#                         if type(el4) == Element:
+#                             print('\t\t\t\t' + el4.tagName)
 
 # сравнение: fc /N tasks.xml filename.xml
 
 
-def parse_domain_props(props=None):
-    props_dict = {}
-    for prop in props.split(', '):
-        if prop == 'show_null':
-            props_dict['show_null'] = True
-        elif prop == 'show_lead_nulls':
-            props_dict['show_lead_nulls'] = True
-        elif prop == 'thousands_separator':
-            props_dict['thousands_separator'] = True
-        elif prop == 'summable':
-            props_dict['summable'] = True
-        elif prop == 'case_sensitive':
-            props_dict['case_sensitive'] = True
-    return props_dict
+def parse_dbd_schema(attributes):
+    if set(attributes.keys()) <= {'fulltext_engine', 'version',
+                                  'name', 'description'}:
+        return attributes
+    else:
+        raise Exception
 
 
 def parse_domain(attributes):
-    if set(attributes.keys()) < {'name', 'type', 'description', 'data_type_id',
+    if set(attributes.keys()) <= {'name', 'type', 'description', 'data_type_id',
                                  'length', 'char_length', 'precision',
                                  'scale', 'width', 'align', 'props'}:
         if 'props' in attributes.keys():
-            attributes.update(parse_domain_props(attributes['props']))
+            def parse_domain_props():
+                props_dict = {}
+                for prop in attributes['props'].split(', '):
+                    if prop == 'show_null':
+                        props_dict['show_null'] = True
+                    elif prop == 'show_lead_nulls':
+                        props_dict['show_lead_nulls'] = True
+                    elif prop == 'thousands_separator':
+                        props_dict['thousands_separator'] = True
+                    elif prop == 'summable':
+                        props_dict['summable'] = True
+                    elif prop == 'case_sensitive':
+                        props_dict['case_sensitive'] = True
+                return props_dict
+            attributes.update(parse_domain_props())
         return attributes
     else:
         raise Exception
-
-
-def parse_table_props(props=None):
-    props_dict = {}
-    for prop in props.split(', '):
-        if prop == 'add':
-            props_dict['can_add'] = True
-        elif prop == 'edit':
-            props_dict['can_edit'] = True
-        elif prop == 'delete':
-            props_dict['can_delete'] = True
-    return props_dict
 
 
 def parse_table(attributes):
-    if set(attributes.keys()) > {'name', 'description', 'temporal_mode', 'means', 'props'}:
-        attributes.update(parse_table_props(attributes['props']))
+    if set(attributes.keys()) <= {'name', 'description', 'temporal_mode',
+                                  'means', 'props'}:
+        if 'props' in attributes.keys():
+            def parse_table_props():
+                props_dict = {}
+                for prop in attributes['props'].split(', '):
+                    if prop == 'add':
+                        props_dict['can_add'] = True
+                    elif prop == 'edit':
+                        props_dict['can_edit'] = True
+                    elif prop == 'delete':
+                        props_dict['can_delete'] = True
+                return props_dict
+            attributes.update(parse_table_props())
         return attributes
     else:
         raise Exception
 
 
-def parse_field_props(props=None):
-    props_dict = {}
-    for prop in props.split(', '):
-        if prop == 'input':
-            props_dict['can_input'] = True
-        elif prop == 'edit':
-            props_dict['can_edit'] = True
-        elif prop == 'show_in_grid':
-            props_dict['show_in_grid'] = True
-        elif prop == 'show_in_details':
-            props_dict['show_in_details'] = True
-        elif prop == 'is_mean':
-            props_dict['is_mean'] = True
-        elif prop == 'autocalculated':
-            props_dict['autocalculated'] = True
-        elif prop == 'required':
-            props_dict['required'] = True
-    return props_dict
-
-
 def parse_field(attributes):
-    if set(attributes.keys()) > {'position', 'name', 'rname', 'description', 'domain', 'props'}:
-        attributes.update(parse_field_props(attributes['props']))
+    if set(attributes.keys()) <= {'position', 'name', 'rname',
+                                  'description', 'domain', 'props'}:
+        def parse_field_props():
+            props_dict = {}
+            for prop in attributes['props'].split(', '):
+                if prop == 'input':
+                    props_dict['can_input'] = True
+                elif prop == 'edit':
+                    props_dict['can_edit'] = True
+                elif prop == 'show_in_grid':
+                    props_dict['show_in_grid'] = True
+                elif prop == 'show_in_details':
+                    props_dict['show_in_details'] = True
+                elif prop == 'is_mean':
+                    props_dict['is_mean'] = True
+                elif prop == 'autocalculated':
+                    props_dict['autocalculated'] = True
+                elif prop == 'required':
+                    props_dict['required'] = True
+            return props_dict
+        attributes.update(parse_field_props())
         attributes['russian_short_name'] = attributes.pop('rname')
         return attributes
     else:
         raise Exception
 
 
-def parse_const_props(props=None):
-    props_dict = {}
-    for prop in props.split(', '):
-        if prop == 'full_cascading_delete':
-            props_dict['cascading_delete'] = True
-        elif prop == 'cascading_delete':
-            props_dict['cascading_delete'] = False
-        elif prop == 'has_value_edit':
-            props_dict['has_value_edit'] = True
-    return props_dict
-    # const_props_dict['constraint_type'] = const_props_dict['kind']
-
-
-def parse_const(attributes):
-    if set(attributes.keys()) > {'name', 'constraint_type', 'reference', 'props', 'expression',
-                                 'kind', 'items'}:
-        attributes.update(parse_const_props(attributes['props']))
+def parse_constraint(attributes):
+    if set(attributes.keys()) <= {'name', 'constraint_type', 'reference',
+                                  'props', 'expression', 'kind', 'items'}:
+        if 'props' in attributes.keys():
+            def parse_const_props():
+                props_dict = {}
+                for prop in attributes['props'].split(', '):
+                    if prop == 'full_cascading_delete':
+                        props_dict['cascading_delete'] = True
+                    elif prop == 'cascading_delete':
+                        props_dict['cascading_delete'] = False
+                    elif prop == 'has_value_edit':
+                        props_dict['has_value_edit'] = True
+                return props_dict
+            attributes.update(parse_const_props())
+        attributes['constraint_type'] = attributes.pop('kind')
         return attributes
     else:
         raise Exception
-
-
-def parse_index_props(props=None):
-    if props is not None:
-        return {'kind': props}
-    else:
-        {}
 
 
 def parse_index(attributes):
-    if set(attributes.keys()) > {'name', 'props', 'kind', 'field'}:
-        attributes.update(parse_index_props(attributes['props']))
+    if set(attributes.keys()) <= {'name', 'props', 'kind', 'field'}:
+        if 'props' in attributes.keys():
+            def parse_index_props():
+                return {'kind': attributes['props']}
+            attributes.update(parse_index_props())
+        attributes['name'] = attributes.pop('field')
         return attributes
     else:
         raise Exception
 
 
-def create_object(obj, attr, parse_func):
-    obj.set_attributes(parse_func(dict(attr.attributes.items())))
+def get_parse_func(node_name):
+    try:
+        return globals()['parse_'+node_name]
+    except Exception as e:
+        raise e
+
+
+def get_object_by_name(obj_name):
+    try:
+        return globals()[obj_name.title().replace("_", "")]()
+    except Exception as e:
+        raise e
+
+
+def create_object(obj, xml_attr, parse_func):
+    obj.set_attributes(parse_func(dict(xml_attr.attributes.items())))
     if obj.is_valid():
         return obj
     else:
         raise Exception
 
 
+def prefix_traverse(node):
+    if node.hasChildNodes():
+        for child in node.childNodes:
+            if isinstance(child, Element) and child.hasAttributes():
+                print(child.tagName)
+                create_object(get_object_by_name(child.tagName),
+                              child,
+                              get_parse_func(child.tagName))
+            prefix_traverse(child)
+
 
 def xml_to_ram(filename):
-    # domain_id = table_id = const_id = field_id = index_id = 1
     dom = parse(filename)
+    prefix_traverse(dom)
 
-    domains = []
-    for domain in dom.getElementsByTagName('domain'):
-        domains.append(create_object(Domain(), domain, parse_domain))
-    return {'domains': domains}
+    # domain_id = table_id = const_id = field_id = index_id = 1
+    # domains = []
+    # for domain in dom.getElementsByTagName('domain'):
+    #     domains.append(create_object(Domain(), domain, parse_domain))
+    # return {'domains': domains}
 
     # tables = {}
     #
@@ -279,9 +317,11 @@ def ram_to_xml(database_schema):
     file_handle.close()
 
 
+xml_to_ram('tasks.xml')
+# xml_to_ram('prjadm.xdb.xml')
 # try:
     # ram_to_xml(xml_to_ram('prjadm.xdb.xml'))
-ram_to_xml(xml_to_ram('tasks.xml'))
+# ram_to_xml(xml_to_ram('tasks.xml'))
     # ram_to_xml(xml_to_ram('tst.xml'))
 # except Exception as e:
 #     print(e)
--- metadata.py
+++ metadata.py
@@ -17,7 +17,7 @@ import copy
 #             raise Exception
 
 
-class Schema:
+class DbdSchema:
     def __init__(self):
         self.fulltext_engine = None
         self.version = None
@@ -25,10 +25,14 @@ class Schema:
         self.description = None
 
     def set_attributes(self, init_dict):
-        self.fulltext_engine = init_dict['fulltext_engine']
-        self.version = init_dict['version']
-        self.name = init_dict['name']
-        self.description = init_dict['description']
+        for key, value in self.__dict__.items():
+            if key in init_dict.keys():
+                self.__dict__[key] = init_dict[key]
+
+    def is_valid(self):
+        if not self.name:
+            return False
+        return True
 
 
 class Domain:
@@ -79,23 +83,22 @@ class Domain:
 
     def is_valid(self):
         if not self.name:
-            self.__dict__ = None
             return False
         return True
 
 
 class Table:
-    def __init__(self, init_dict):
-        self.id = init_dict['id']
-        self.schema_id = init_dict['schema_id']
-        self.name = init_dict['name']
-        self.description = init_dict['description']
-        self.can_add = init_dict['can_add']
-        self.can_edit = init_dict['can_edit']
-        self.can_delete = init_dict['can_delete']
-        self.temporal_mode = init_dict['temporal_mode']
-        self.means = init_dict['means']
-        self.uuid = init_dict['uuid']
+    def __init__(self):
+        self.id = None
+        self.schema_id = None
+        self.name = None
+        self.description = None
+        self.can_add = None
+        self.can_edit = None
+        self.can_delete = None
+        self.temporal_mode = None
+        self.means = None
+        self.uuid = None
 
     def get_attributes(self):
         dict_order = {'name': None,
@@ -111,24 +114,34 @@ class Table:
             dict_order['props'] = s[:-2]
         return {k: v for k, v in dict_order.items() if v is not None and k[-2:] != "id"}
 
+    def set_attributes(self, init_dict):
+        for key, value in self.__dict__.items():
+            if key in init_dict.keys():
+                self.__dict__[key] = init_dict[key]
+
+    def is_valid(self):
+        if not self.name:
+            return False
+        return True
+
 
 class Field:
-    def __init__(self, init_dict):
-        self.id = init_dict['id']
-        self.table_id = init_dict['table_id']
-        self.position = init_dict['position']
-        self.name = init_dict['name']
-        self.russian_short_name = init_dict['russian_short_name']
-        self.description = init_dict['description']
-        self.domain_id = init_dict['domain_id']
-        self.can_input = init_dict['can_input']
-        self.can_edit = init_dict['can_edit']
-        self.show_in_grid = init_dict['show_in_grid']
-        self.show_in_details = init_dict['show_in_details']
-        self.is_mean = init_dict['is_mean']
-        self.autocalculated = init_dict['autocalculated']
-        self.required = init_dict['required']
-        self.uuid = init_dict['uuid']
+    def __init__(self):
+        self.id = None
+        self.table_id = None
+        self.position = None
+        self.name = None
+        self.russian_short_name = None
+        self.description = None
+        self.domain_id = None
+        self.can_input = None
+        self.can_edit = None
+        self.show_in_grid = None
+        self.show_in_details = None
+        self.is_mean = None
+        self.autocalculated = None
+        self.required = None
+        self.uuid = None
 
     def get_attributes(self):
         s = ""
@@ -152,19 +165,29 @@ class Field:
         dict_order.update(new_dict)
         return {k: v for k, v in dict_order.items() if v is not None and k[-2:] != "id"}
 
+    def set_attributes(self, init_dict):
+        for key, value in self.__dict__.items():
+            if key in init_dict.keys():
+                self.__dict__[key] = init_dict[key]
+
+    def is_valid(self):
+        if not self.name:
+            return False
+        return True
+
 
 class Constraint:
-    def __init__(self, init_dict):
-        self.id = init_dict['id']
-        self.table_id = init_dict['table_id']
-        self.name = init_dict['name']
-        self.constraint_type = init_dict['constraint_type']
-        self.reference = init_dict['reference']
-        self.unique_key_id = init_dict['unique_key_id']
-        self.has_value_edit = init_dict['has_value_edit']
-        self.cascading_delete = init_dict['cascading_delete']
-        self.expression = init_dict['expression']
-        self.uuid = init_dict['uuid']
+    def __init__(self):
+        self.id = None
+        self.table_id = None
+        self.name = None
+        self.constraint_type = None
+        self.reference = None
+        self.unique_key_id = None
+        self.has_value_edit = None
+        self.cascading_delete = None
+        self.expression = None
+        self.uuid = None
 
     def get_attributes(self):
         new_dict = {}
@@ -191,13 +214,23 @@ class Constraint:
         dict_order.update(new_dict)
         return {k: v for k, v in dict_order.items() if v is not None}
 
+    def set_attributes(self, init_dict):
+        for key, value in self.__dict__.items():
+            if key in init_dict.keys():
+                self.__dict__[key] = init_dict[key]
+
+    def is_valid(self):
+        if not self.constraint_type:
+            return False
+        return True
+
 
 class ConstraintDetail:
-    def __init__(self, init_dict):
-        self.id = init_dict['id']
-        self.constraint_id = init_dict['constraint_id']
-        self.position = init_dict['position']
-        self.field_id = init_dict['field_id']
+    def __init__(self):
+        self.id = None
+        self.constraint_id = None
+        self.position = None
+        self.field_id = None
 
     def is_valid(self):
         if not self.constraint_id or not self.field_id:
@@ -205,15 +238,14 @@ class ConstraintDetail:
             raise Exception
 
 
-
 class Index:
-    def __init__(self, init_dict):
-        self.id = init_dict['id']
-        self.table_id = init_dict['table_id']
-        self.name = init_dict['name']
-        self.local = init_dict['local']
-        self.kind = init_dict['kind']
-        self.uuid = init_dict['uuid']
+    def __init__(self):
+        self.id = None
+        self.table_id = None
+        self.name = None
+        self.local = None
+        self.kind = None
+        self.uuid = None
 
     def get_attributes(self):
         new_dict = copy.copy(self.__dict__)
@@ -221,18 +253,32 @@ class Index:
         new_dict['props'] = new_dict.pop('kind')
         return {k: v for k, v in new_dict.items() if v is not None and k[-2:] != "id"}
 
+    def set_attributes(self, init_dict):
+        for key, value in self.__dict__.items():
+            if key in init_dict.keys():
+                self.__dict__[key] = init_dict[key]
+
+    def is_valid(self):
+        if not self.name:
+            return False
+        return True
+
 
 class IndexDetail:
-    def __init__(self, id=None, index_id=None, position=None,
-                 field_id=None, expression=None, descend=None):
-        self.id = id
-        self.index_id = index_id
-        self.position = position
-        self.field_id = field_id
-        self.expression = expression
-        self.descend = descend
+    def __init__(self):
+        self.id = None
+        self.index_id = None
+        self.position = None
+        self.field_id = None
+        self.expression = None
+        self.descend = None
 
     def is_valid(self):
         if not self.index_id or not self.field_id:
             self.__dict__ = None
             raise Exception
+
+    def set_attributes(self, init_dict):
+        for key, value in self.__dict__.items():
+            if key in init_dict.keys():
+                self.__dict__[key] = init_dict[key]
