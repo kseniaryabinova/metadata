8e47a5184f0b66f0c363f68bdbf8bab85940db00
kseniaryabinova <ryabinova.ka@mail.ru>
2017-12-16 13:48:36 +0300

    рефакторинг

--- main.py
+++ main.py
@@ -5,12 +5,3 @@ reader = Reader('prjadm.xdb.xml')
 generator = RAMtoSQLite(reader.xml_to_ram())
 generator.generate()
 
-
-"""
-крч, sqlite нужен, чтобы к нему быстро запросы писать, а не ходить по объектам в словарях
-или не грузить сервак
-схема загрузки будет такая:
-xml -> ram -> sqlite
-sqlite -> ram -> ddl инструкции -> postgres
-ms sql -> ram -> sqlite -> ram -> ddl инструкции -> postgres  
-"""
